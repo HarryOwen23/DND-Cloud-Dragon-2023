714c5a44d22646f54515f0a66839a5c0f38da90d
Harry Owen <harry.owen@elastacloud.com>
2023-11-10 08:40:08 +0000

    Current Spell casting classes json loaders are complete.

---
+++ CloudDragon/Warlock_Spell_+_Cantrips_Json_Loader.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using CloudDragon;
+
+namespace CloudDragon
+{
+    public class WarlockCantrips
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("Source")]
+        public string Source { get; set; }
+
+        [JsonPropertyName("School")]
+        public string School { get; set; }
+
+        [JsonPropertyName("Casting Time")]
+        public string Cast_Time { get; set; }
+
+        [JsonPropertyName("Range")]
+        public string Range { get; set; }
+
+        [JsonPropertyName("Components")]
+        public string Components { get; set; }
+
+        [JsonPropertyName("Duration")]
+        public string Duration { get; set; }
+
+        [JsonPropertyName("Description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("Spell Lists")]
+        public string Spell_Lists { get; set; }
+    }
+    public class WarlockSpells
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("School")]
+        public string School { get; set; }
+
+        [JsonPropertyName("Description")]
+        public string Description { get; set; }
+    }
+
+    public class WarlockCantripcategory
+    {
+        [JsonPropertyName("Cantrips")]
+        public List<WarlockCantrips> Cantrips { get; set; }
+    }
+
+    public class WarlockSpellcategory
+    {
+        [JsonPropertyName("Spells")]
+        public List<WarlockSpells> Spells { get; set; }
+    }
+
+    public class WarlockCantripData
+    {
+        [JsonPropertyName("Cantrip Categories")]
+        public Dictionary<string, List<WarlockCantrips>> CantripCategories { get; set; }
+    }
+
+    public class WarlockSpellData
+{
+        [JsonPropertyName("Spell Categories")]
+        public Dictionary<string, List<WarlockSpells>> SpellCategories { get; set; }
+    }
+
+    internal class Warlock_Cantrips_Json_Loader
+{
+        public static WarlockCantripData LoadwarlockCantripData(string jsonFilePath)
+        {
+            try
+            {
+                string jsonData = File.ReadAllText(jsonFilePath);
+                var warlockCantripCategory = JsonSerializer.Deserialize<WarlockCantripcategory>(jsonData);
+                return new WarlockCantripData
+                {
+                    CantripCategories = new Dictionary<string, List<WarlockCantrips>>
+                    {
+                        { Path.GetFileNameWithoutExtension(jsonFilePath), warlockCantripCategory.Cantrips }
+                    }
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error loading JSON file: " + e.Message);
+                return null;
+            }
+        }
+    }
+
+    internal class Warlock_Spells_Json_Loader
+    {
+        public static WarlockSpellData LoadwarlockSpellData(string jsonFilePath)
+        {
+            try
+            {
+                string jsonData = File.ReadAllText(jsonFilePath);
+                var warlockSpellCategory = JsonSerializer.Deserialize<WarlockSpellcategory>(jsonData);
+                return new WarlockSpellData
+                {
+                    SpellCategories = new Dictionary<string, List<WarlockSpells>>
+                    {
+                        { Path.GetFileNameWithoutExtension(jsonFilePath), warlockSpellCategory.Spells }
+                    }
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error loading JSON file: " + e.Message);
+                return null;
+            }
+        }
+    }
+
+    internal class WarlockCantripLoader : ILoader
+    {
+        void ILoader.Load()
+        {
+            Console.WriteLine("Loading Warlock Cantrip Data ...");
+            // Define the paths to the JSON files
+            string jsonFilePathWarlockCantrip = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Cantrips.json";
+
+            var cantripsWarlock = Warlock_Cantrips_Json_Loader.LoadwarlockCantripData(jsonFilePathWarlockCantrip);
+
+
+            // Display the data for Warlock Cantrips
+            if (cantripsWarlock != null)
+            {
+                Console.WriteLine("Warlock Cantrips:");
+                foreach (var warlockCans in cantripsWarlock.CantripCategories["Warlock Cantrips"])
+                {
+                    Console.WriteLine($"- Name: {warlockCans.Name}, Source: {warlockCans.Source}, School: {warlockCans.School}, Cast_Time: {warlockCans.Cast_Time}, Components: {warlockCans.Components}, Duration: {warlockCans.Duration}, Description: {warlockCans.Description}, Spell_Lists: {warlockCans.Spell_Lists} ");
+                }
+            }
+        }
+    }
+
+    internal class WarlockSpellLoader : ILoader
+    {
+        void ILoader.Load()
+        {
+            Console.WriteLine("Loading Warlock Spell Data");
+            // Define paths to the Warlock spell Json files
+            string jsonFilePathWarlockLevel1 = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Level_1_Spells.json";
+            string jsonFilePathWarlockLevel2 = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Level_2_Spells.json";
+            string jsonFilePathWarlockLevel3 = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Level_3_Spells.json";
+            string jsonFilePathWarlockLevel4 = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Level_4_Spells.json";
+            string jsonFilePathWarlockLevel5 = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Level_5_Spells.json";
+            string jsonFilePathWarlockLevel6 = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Level_6_Spells.json";
+            string jsonFilePathWarlockLevel7 = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Level_7_Spells.json";
+            string jsonFilePathWarlockLevel8 = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Level_8_Spells.json";
+            string jsonFilePathWarlockLevel9 = "Spells+Cantrips\\Warlock_Cantrips_+_Spells\\Warlock_Level_9_Spells.json";
+
+            var level1warlockspells = Warlock_Spells_Json_Loader.LoadwarlockSpellData(jsonFilePathWarlockLevel1);
+            var level2warlockspells = Warlock_Spells_Json_Loader.LoadwarlockSpellData(jsonFilePathWarlockLevel2);
+            var level3warlockspells = Warlock_Spells_Json_Loader.LoadwarlockSpellData(jsonFilePathWarlockLevel3);
+            var level4warlockspells = Warlock_Spells_Json_Loader.LoadwarlockSpellData(jsonFilePathWarlockLevel4);
+            var level5warlockspells = Warlock_Spells_Json_Loader.LoadwarlockSpellData(jsonFilePathWarlockLevel5);
+            var level6warlockspells = Warlock_Spells_Json_Loader.LoadwarlockSpellData(jsonFilePathWarlockLevel6);
+            var level7warlockspells = Warlock_Spells_Json_Loader.LoadwarlockSpellData(jsonFilePathWarlockLevel7);
+            var level8warlockspells = Warlock_Spells_Json_Loader.LoadwarlockSpellData(jsonFilePathWarlockLevel8);
+            var level9warlockspells = Warlock_Spells_Json_Loader.LoadwarlockSpellData(jsonFilePathWarlockLevel9);
+
+
+            // Display the data for Level 1 spells
+            if (level1warlockspells != null)
+            {
+                Console.WriteLine("Level 1 Warlock Spells:");
+                foreach (var warlockSpell1 in level1warlockspells.SpellCategories["Level 1 Warlock Spells"])
+                {
+                    Console.WriteLine($"- Name: {warlockSpell1.Name}, School: {warlockSpell1.School}, Description: {warlockSpell1.Description} ");
+                }
+            }
+
+            // Display the data for Level 2 spells
+            if (level2warlockspells != null)
+            {
+                Console.WriteLine("Level 2 Warlock Spells:");
+                foreach (var warlockSpell2 in level2warlockspells.SpellCategories["Level 2 Warlock Spells"])
+                {
+                    Console.WriteLine($"- Name: {warlockSpell2.Name}, School: {warlockSpell2.School}, Description: {warlockSpell2.Description} ");
+                }
+            }
+
+            // Display the data for Level 3 spells
+            if (level3warlockspells != null)
+            {
+                Console.WriteLine("Level 3 Warlock Spells:");
+                foreach (var warlockSpell3 in level3warlockspells.SpellCategories["Level 3 Warlock Spells"])
+                {
+                    Console.WriteLine($"- Name: {warlockSpell3.Name}, School: {warlockSpell3.School} , Description:  {warlockSpell3.Description} ");
+                }
+            }
+
+            // Display the data for Level 4 spells
+            if (level4warlockspells != null)
+            {
+                Console.WriteLine("Level 4 Warlock Spells:");
+                foreach (var warlockSpell4 in level4warlockspells.SpellCategories["Level 4 Warlock Spells"])
+                {
+                    Console.WriteLine($"- Name: {warlockSpell4.Name}, School: {warlockSpell4.School}, Description: {warlockSpell4.Description} ");
+                }
+            }
+
+            // Display the data for Level 5 spells
+            if (level5warlockspells != null)
+            {
+                Console.WriteLine("Level 5 Warlock Spells:");
+                foreach (var warlockSpell5 in level5warlockspells.SpellCategories["Level 5 Warlock Spells"])
+                {
+                    Console.WriteLine($"- Name: {warlockSpell5.Name}, School: {warlockSpell5.School} , Description:  {warlockSpell5.Description} ");
+                }
+            }
+
+            // Display the data for Level 6 spells
+            if (level6warlockspells != null)
+            {
+                Console.WriteLine("Level 6 Warlock Spells:");
+                foreach (var warlockSpell6 in level6warlockspells.SpellCategories["Level 6 Warlock Spells"])
+                {
+                    Console.WriteLine($"- Name: {warlockSpell6.Name}, School: {warlockSpell6.School}  , Description:   {warlockSpell6.Description} ");
+                }
+            }
+
+            // Display the data for Level 7 spells
+            if (level7warlockspells != null)
+            {
+                Console.WriteLine("Level 7 Warlock Spells:");
+                foreach (var warlockSpell7 in level7warlockspells.SpellCategories["Level 7 Warlock Spells"])
+                {
+                    Console.WriteLine($"- Name: {warlockSpell7.Name}, School: {warlockSpell7.School} , Description:  {warlockSpell7.Description} ");
+                }
+            }
+
+            // Display the data for Level 8 spells
+            if (level8warlockspells != null)
+            {
+                Console.WriteLine("Level 8 Warlock Spells:");
+                foreach (var warlockSpell8 in level8warlockspells.SpellCategories["Level 8 Warlock Spells"])
+                {
+                    Console.WriteLine($"- Name: {warlockSpell8.Name}, School: {warlockSpell8.School}, Description: {warlockSpell8.Description} ");
+                }
+            }
+
+            // Display the data for Level 9 spells
+            if (level9warlockspells != null)
+            {
+                Console.WriteLine("Level 9 Warlock Spells:");
+                foreach (var warlockSpell9 in level9warlockspells.SpellCategories["Level 9 Warlock Spells"])
+                {
+                    Console.WriteLine($"- Name: {warlockSpell9.Name}, School: {warlockSpell9.School}, Description: {warlockSpell9.Description} ");
+                }
+            }
+        }
+    }
+}
---
+++ CloudDragon/Wizard_Spell+Cantrips_Json_Loader.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using CloudDragon;
+
+namespace CloudDragon
+{
+    public class WizardCantrips
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("Source")]
+        public string Source { get; set; }
+
+        [JsonPropertyName("School")]
+        public string School { get; set; }
+
+        [JsonPropertyName("Casting Time")]
+        public string Cast_Time { get; set; }
+
+        [JsonPropertyName("Range")]
+        public string Range { get; set; }
+
+        [JsonPropertyName("Components")]
+        public string Components { get; set; }
+
+        [JsonPropertyName("Duration")]
+        public string Duration { get; set; }
+
+        [JsonPropertyName("Description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("Spell Lists")]
+        public string Spell_Lists { get; set; }
+    }
+    public class WizardSpells
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("School")]
+        public string School { get; set; }
+
+        [JsonPropertyName("Description")]
+        public string Description { get; set; }
+    }
+
+    public class WizardCantripcategory
+    {
+        [JsonPropertyName("Cantrips")]
+        public List<WizardCantrips> Cantrips { get; set; }
+    }
+
+    public class WizardSpellcategory
+    {
+        [JsonPropertyName("Spells")]
+        public List<WizardSpells> Spells { get; set; }
+    }
+
+    public class WizardCantripData
+    {
+        [JsonPropertyName("Cantrip Categories")]
+        public Dictionary<string, List<WizardCantrips>> CantripCategories { get; set; }
+    }
+
+    public class WizardSpellData
+    {
+        [JsonPropertyName("Spell Categories")]
+        public Dictionary<string, List<WizardSpells>> SpellCategories { get; set; }
+    }
+
+    internal class Wizard_Cantrips_Json_Loader
+    {
+        public static WizardCantripData LoadwizardCantripData(string jsonFilePath)
+        {
+            try
+            {
+                string jsonData = File.ReadAllText(jsonFilePath);
+                var wizardCantripCategory = JsonSerializer.Deserialize<WizardCantripcategory>(jsonData);
+                return new WizardCantripData
+                {
+                    CantripCategories = new Dictionary<string, List<WizardCantrips>>
+                    {
+                        { Path.GetFileNameWithoutExtension(jsonFilePath), wizardCantripCategory.Cantrips }
+                    }
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error loading JSON file: " + e.Message);
+                return null;
+            }
+        }
+    }
+
+    internal class Wizard_Spells_Json_Loader
+    {
+        public static WizardSpellData LoadwizardSpellData(string jsonFilePath)
+        {
+            try
+            {
+                string jsonData = File.ReadAllText(jsonFilePath);
+                var wizardSpellCategory = JsonSerializer.Deserialize<WizardSpellcategory>(jsonData);
+                return new WizardSpellData
+                {
+                    SpellCategories = new Dictionary<string, List<WizardSpells>>
+                    {
+                        { Path.GetFileNameWithoutExtension(jsonFilePath), wizardSpellCategory.Spells }
+                    }
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error loading JSON file: " + e.Message);
+                return null;
+            }
+        }
+    }
+
+    internal class WizardCantripLoader : ILoader
+    {
+        void ILoader.Load()
+        {
+            Console.WriteLine("Loading Wizard Cantrip Data ...");
+            // Define the paths to the JSON files
+            string jsonFilePathWizardCantrip = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Wizard_Cantrips.json";
+
+            var cantripsWizard = Wizard_Cantrips_Json_Loader.LoadwizardCantripData(jsonFilePathWizardCantrip);
+
+
+            // Display the data for Wizard Cantrips
+            if (cantripsWizard != null)
+            {
+                Console.WriteLine("Wizard Cantrips:");
+                foreach (var wizardCans in cantripsWizard.CantripCategories["Wizard Cantrips"])
+                {
+                    Console.WriteLine($"- Name: {wizardCans.Name}, Source: {wizardCans.Source}, School: {wizardCans.School}, Cast_Time: {wizardCans.Cast_Time}, Components: {wizardCans.Components}, Duration: {wizardCans.Duration}, Description: {wizardCans.Description}, Spell_Lists: {wizardCans.Spell_Lists} ");
+                }
+            }
+        }
+    }
+
+    internal class WizardSpellLoader : ILoader
+    {
+        void ILoader.Load()
+        {
+            Console.WriteLine("Loading Wizard Spell Data");
+            // Define paths to the Wizard spell Json files
+            string jsonFilePathWizardLevel1 = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Wizard_Level_1_Spells.json";
+            string jsonFilePathWizardLevel2 = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Wizard_Level_2_Spells.json";
+            string jsonFilePathWizardLevel3 = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Wizard_Level_3_Spells.json";
+            string jsonFilePathWizardLevel4 = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Wizard_Level_4_Spells.json";
+            string jsonFilePathWizardLevel5 = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Wizard_Level_5_Spells.json";
+            string jsonFilePathWizardLevel6 = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Wizard_Level_6_Spells.json";
+            string jsonFilePathWizardLevel7 = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Waizard_Level_7_Spells.json";
+            string jsonFilePathWizardLevel8 = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Wizard_Level_8_Spells.json";
+            string jsonFilePathWizardLevel9 = "Spells+Cantrips\\Wizard_Cantrips_+_Spells\\Wizard_Level_9_Spells.json";
+
+            var level1wizardspells = Wizard_Spells_Json_Loader.LoadwizardSpellData(jsonFilePathWizardLevel1);
+            var level2wizardspells = Wizard_Spells_Json_Loader.LoadwizardSpellData(jsonFilePathWizardLevel2);
+            var level3wizardspells = Wizard_Spells_Json_Loader.LoadwizardSpellData(jsonFilePathWizardLevel3);
+            var level4wizardspells = Wizard_Spells_Json_Loader.LoadwizardSpellData(jsonFilePathWizardLevel4);
+            var level5wizardspells = Wizard_Spells_Json_Loader.LoadwizardSpellData(jsonFilePathWizardLevel5);
+            var level6wizardspells = Wizard_Spells_Json_Loader.LoadwizardSpellData(jsonFilePathWizardLevel6);
+            var level7wizardspells = Wizard_Spells_Json_Loader.LoadwizardSpellData(jsonFilePathWizardLevel7);
+            var level8wizardspells = Wizard_Spells_Json_Loader.LoadwizardSpellData(jsonFilePathWizardLevel8);
+            var level9wizardspells = Wizard_Spells_Json_Loader.LoadwizardSpellData(jsonFilePathWizardLevel9);
+
+
+            // Display the data for Level 1 spells
+            if (level1wizardspells != null)
+            {
+                Console.WriteLine("Level 1 Wizard Spells:");
+                foreach (var wizardSpell1 in level1wizardspells.SpellCategories["Level 1 Wizard Spells"])
+                {
+                    Console.WriteLine($"- Name: {wizardSpell1.Name}, School: {wizardSpell1.School}, Description: {wizardSpell1.Description} ");
+                }
+            }
+
+            // Display the data for Level 2 spells
+            if (level2wizardspells != null)
+            {
+                Console.WriteLine("Level 2 Wizard Spells:");
+                foreach (var wizardSpell2 in level2wizardspells.SpellCategories["Level 2 Wizard Spells"])
+                {
+                    Console.WriteLine($"- Name: {wizardSpell2.Name}, School: {wizardSpell2.School}, Description: {wizardSpell2.Description} ");
+                }
+            }
+
+            // Display the data for Level 3 spells
+            if (level3wizardspells != null)
+            {
+                Console.WriteLine("Level 3 Wizard Spells:");
+                foreach (var wizardSpell3 in level3wizardspells.SpellCategories["Level 3 Wizard Spells"])
+                {
+                    Console.WriteLine($"- Name: {wizardSpell3.Name}, School: {wizardSpell3.School} , Description:  {wizardSpell3.Description} ");
+                }
+            }
+
+            // Display the data for Level 4 spells
+            if (level4wizardspells != null)
+            {
+                Console.WriteLine("Level 4 Wizard Spells:");
+                foreach (var wizardSpell4 in level4wizardspells.SpellCategories["Level 4 Wizard Spells"])
+                {
+                    Console.WriteLine($"- Name: {wizardSpell4.Name}, School: {wizardSpell4.School}, Description: {wizardSpell4.Description} ");
+                }
+            }
+
+            // Display the data for Level 5 spells
+            if (level5wizardspells != null)
+            {
+                Console.WriteLine("Level 5 Wizard Spells:");
+                foreach (var wizardSpell5 in level5wizardspells.SpellCategories["Level 5 Wizard Spells"])
+                {
+                    Console.WriteLine($"- Name: {wizardSpell5.Name}, School: {wizardSpell5.School} , Description:  {wizardSpell5.Description} ");
+                }
+            }
+
+            // Display the data for Level 6 spells
+            if (level6wizardspells != null)
+            {
+                Console.WriteLine("Level 6 Wizard Spells:");
+                foreach (var wizardSpell6 in level6wizardspells.SpellCategories["Level 6 Wizard Spells"])
+                {
+                    Console.WriteLine($"- Name: {wizardSpell6.Name}, School: {wizardSpell6.School}  , Description:   {wizardSpell6.Description} ");
+                }
+            }
+
+            // Display the data for Level 7 spells
+            if (level7wizardspells != null)
+            {
+                Console.WriteLine("Level 7 Wizard Spells:");
+                foreach (var wizardSpell7 in level7wizardspells.SpellCategories["Level 7 Wizard Spells"])
+                {
+                    Console.WriteLine($"- Name: {wizardSpell7.Name}, School: {wizardSpell7.School} , Description:  {wizardSpell7.Description} ");
+                }
+            }
+
+            // Display the data for Level 8 spells
+            if (level8wizardspells != null)
+            {
+                Console.WriteLine("Level 8 Wizard Spells:");
+                foreach (var wizardSpell8 in level8wizardspells.SpellCategories["Level 8 Wizard Spells"])
+                {
+                    Console.WriteLine($"- Name: {wizardSpell8.Name}, School: {wizardSpell8.School}, Description: {wizardSpell8.Description} ");
+                }
+            }
+
+            // Display the data for Level 9 spells
+            if (level9wizardspells != null)
+            {
+                Console.WriteLine("Level 9 Wizard Spells:");
+                foreach (var wizardSpell9 in level9wizardspells.SpellCategories["Level 9 Wizard Spells"])
+                {
+                    Console.WriteLine($"- Name: {wizardSpell9.Name}, School: {wizardSpell9.School}, Description: {wizardSpell9.Description} ");
+                }
+            }
+        }
+    }
+}
