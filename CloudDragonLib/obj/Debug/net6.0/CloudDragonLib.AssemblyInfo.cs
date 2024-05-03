70b8f4531f34a2cc4e24ed3ee92b74c30bde3d6b
Harry Owen <harry.owen@elastacloud.com>
2024-05-03 17:27:46 +0100

    Paladin, Ranger, Sorcerer and Warlock spells added into single json files.

--- CloudDragonLib/obj/Debug/net6.0/CloudDragonLib.AssemblyInfo.cs
+++ CloudDragonLib/obj/Debug/net6.0/CloudDragonLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudDragonLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f52a447188da9df08ff580885fa79154a1c59436")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0f974403e642fb534c2cc2edef28f99389aaa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudDragonLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudDragonLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
