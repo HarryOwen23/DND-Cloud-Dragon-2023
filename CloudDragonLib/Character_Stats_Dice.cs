8917bbbc54bd6aeeee07121ac8f9c53eb52e041a
HarryOwen23 <123649385+HarryOwen23@users.noreply.github.com>
2025-06-05 09:38:55 +0100

    Fix naming and configuration issues

--- CloudDragonLib/Character_Stats_Dice.cs
+++ CloudDragonLib/Character_Stats_Dice.cs
@@ -1,65 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 
-// a class created to provide RNG for individual dnd stats 
+/// <summary>
+/// Utility for generating random ability scores using the standard 4d6 drop lowest method.
+/// </summary>
 public class CharStats
 {
-    // Private readonly class to retrieve the stats generated
-	private static Random statsrng;
+    private static readonly Random rng = new();
 
-	// The public variables stats are created
-	public int Strength { get; }
+    public int Strength { get; }
     public int Dexterity { get; }
     public int Constitution { get; }
     public int Intelligence { get; }
     public int Wisdom { get; }
     public int Charisma { get; }
-    public int Hitpoints { get; }
-    public int Modifier { get; }
 
-    
-    public CharStats (int Str, int Dex, int Con, int Int, int Wis, int Cha)
+    public int Hitpoints => 10 + Modifier(Constitution);
+
+    public CharStats(int str, int dex, int con, int intel, int wis, int cha)
     {
-        statsrng = new Random(DateTime.UtcNow.Millisecond);
-        Strength = Str;
-        Dexterity = Dex;
-        Constitution = Con;
-        Intelligence = Int;
-        Wisdom = Wis;
-        Charisma = Cha;
-        
+        Strength = str;
+        Dexterity = dex;
+        Constitution = con;
+        Intelligence = intel;
+        Wisdom = wis;
+        Charisma = cha;
     }
 
-    // Modifier variable 
-    public static int Modify(int score) => (int)Math.Floor((score - 10) * 0.5);
+    public static int Modifier(int score) => (int)Math.Floor((score - 10) / 2.0);
 
-    // Variable RollD6 is created as an RNG
-    private static int RollD6() => statsrng.Next(1, 7);
+    private static int RollD6() => rng.Next(1, 7);
 
-    // Storring variable Dice
-    public static int AbilityScore() =>
-        // Will roll 4 times and total highest 3
-        new[] { RollD6(), RollD6(), RollD6(), RollD6() }
-        .OrderByDescending(d => d)
-        .Take(3)
-        .Sum();
-
-    // Character Generator Function 
-    public static CharGen Generate()
+    public static int AbilityScore()
     {
-        return new CharGen(
-            AbilityScore(), AbilityScore(),
-            AbilityScore(), AbilityScore(),
-            AbilityScore(), AbilityScore()
-        );
+        return Enumerable.Range(0, 4)
+            .Select(_ => RollD6())
+            .OrderByDescending(d => d)
+            .Take(3)
+            .Sum();
     }
-}
-public class CharGen
-{
-    public CharGen(int score_1, int score_2, int score_3, int score_4, int score_5, int score_6)
+
+    public static CharStats Generate()
     {
-        
+        return new CharStats(
+            AbilityScore(), AbilityScore(),
+            AbilityScore(), AbilityScore(),
+            AbilityScore(), AbilityScore());
     }
 }
