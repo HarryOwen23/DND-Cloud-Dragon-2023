9846a6be086bd74be6273aec8895c06fb5cb2691
HarryOwen23 <123649385+HarryOwen23@users.noreply.github.com>
2025-06-05 10:15:01 +0100

    Fix missing using directives

--- CloudDragon/CloudDragonApi/Functions/Character/Services/SpellcastingService.cs
+++ CloudDragon/CloudDragonApi/Functions/Character/Services/SpellcastingService.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using CloudDragonLib.Models;
 
 namespace CloudDragonApi.Services
 {
