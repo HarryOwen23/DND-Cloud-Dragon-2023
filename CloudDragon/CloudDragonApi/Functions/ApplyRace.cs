5796720cf390e79a5d9edd4eb48dab2e9268b3c1
HarryOwen23 <123649385+HarryOwen23@users.noreply.github.com>
2025-06-06 16:59:45 +0100

    Add debug logging to race functions

--- CloudDragon/CloudDragonApi/Functions/ApplyRace.cs
+++ CloudDragon/CloudDragonApi/Functions/ApplyRace.cs
@@ -8,7 +8,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using CloudDragonLib.Models; // Assuming RacesPopulator lives here
+using CloudDragonLib.Models;
+using CloudDragonApi.Utils;
 
 namespace CloudDragonApi.Functions.Character
 {
@@ -30,26 +31,38 @@ namespace CloudDragonApi.Functions.Character
                 Connection = "CosmosDBConnection")] IAsyncCollector<CloudDragonLib.Models.Character> characterOut,
             ILogger log)
         {
+            DebugLogger.Log($"ApplyRace triggered for {id}");
             string body = await new StreamReader(req.Body).ReadToEndAsync();
+            DebugLogger.Log($"Request Body: {body}");
             var data = JsonConvert.DeserializeObject<dynamic>(body);
             string raceName = data?.race;
 
             if (string.IsNullOrWhiteSpace(raceName))
+            {
+                DebugLogger.Log("Race not provided in request");
                 return new BadRequestObjectResult(new { success = false, error = "Race not provided." });
+            }
 
             var populator = new RacesPopulator();
             var races = await populator.Populate("races.json");
+            DebugLogger.Log($"Loaded {races.Count} races");
             var race = races.FirstOrDefault(r => r.Name.Equals(raceName, StringComparison.OrdinalIgnoreCase));
 
             if (character == null)
+            {
+                DebugLogger.Log($"Character {id} not found");
                 return new NotFoundObjectResult(new { success = false, error = "Character not found." });
+            }
 
             if (race == null)
+            {
+                DebugLogger.Log($"Race not found: {raceName}");
                 return new NotFoundObjectResult(new { success = false, error = "Race not found." });
+            }
 
             character.Stats ??= new Dictionary<string, int>();
 
-            foreach (var bonus in race.abilityBonuses)
+            foreach (var bonus in race.AbilityBonuses)
             {
                 if (character.Stats.ContainsKey(bonus.Key))
                     character.Stats[bonus.Key] += bonus.Value;
@@ -59,6 +72,8 @@ namespace CloudDragonApi.Functions.Character
 
             character.Race = race.Name;
             await characterOut.AddAsync(character);
+
+            DebugLogger.Log($"Applied race {race.Name} to {id}");
 
             return new OkObjectResult(new { success = true, race = race.Name, stats = character.Stats });
         }
--- CloudDragonLib/raceModifier.cs
+++ CloudDragonLib/raceModifier.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CloudDragonLib.Models;
 
 class raceModifier
 {
@@ -6,264 +7,264 @@ class raceModifier
     {
         // Creating races and adding ability score bonuses
         Races humanRace = new Races("Human");
-        humanRace.Add_ability_Bonuses("STR", 1);
-        humanRace.Add_ability_Bonuses("DEX", 1);
-        humanRace.Add_ability_Bonuses("CON", 1);
-        humanRace.Add_ability_Bonuses("INT", 1);
-        humanRace.Add_ability_Bonuses("WIS", 1);
-        humanRace.Add_ability_Bonuses("CHA", 1);
+        humanRace.AddAbilityBonus("STR", 1);
+        humanRace.AddAbilityBonus("DEX", 1);
+        humanRace.AddAbilityBonus("CON", 1);
+        humanRace.AddAbilityBonus("INT", 1);
+        humanRace.AddAbilityBonus("WIS", 1);
+        humanRace.AddAbilityBonus("CHA", 1);
 
         // Dwarf Races
         Races dwarfHillRace = new Races("Dwarf (Hill)");
-        dwarfHillRace.Add_ability_Bonuses("CON", 2);
+        dwarfHillRace.AddAbilityBonus("CON", 2);
 
         Races dwarfMountainRace = new Races("Dwarf (Mountain)");
-        dwarfMountainRace.Add_ability_Bonuses("STR", 2);
-        dwarfHillRace.Add_ability_Bonuses("CON", 2);
+        dwarfMountainRace.AddAbilityBonus("STR", 2);
+        dwarfHillRace.AddAbilityBonus("CON", 2);
 
         Races dwarfGrayRace = new Races("Dwarf (Gray/Duergar)");
-        dwarfGrayRace.Add_ability_Bonuses("STR", 1);
-        dwarfGrayRace.Add_ability_Bonuses("CON", 2);
+        dwarfGrayRace.AddAbilityBonus("STR", 1);
+        dwarfGrayRace.AddAbilityBonus("CON", 2);
 
         Races dwarfDuergarRace = new Races("Dwarf (Duergar)");
-        dwarfDuergarRace.Add_ability_Bonuses("STR", 1);
-        dwarfDuergarRace.Add_ability_Bonuses("CON", 2);
+        dwarfDuergarRace.AddAbilityBonus("STR", 1);
+        dwarfDuergarRace.AddAbilityBonus("CON", 2);
 
         // Elf Races
         Races elfHighRace = new Races("Elf (High)");
-        elfHighRace.Add_ability_Bonuses("DEX", 2);
-        elfHighRace.Add_ability_Bonuses("INT", 1);
+        elfHighRace.AddAbilityBonus("DEX", 2);
+        elfHighRace.AddAbilityBonus("INT", 1);
 
         Races elfWoodRace = new Races("Elf (Wood)");
-        elfWoodRace.Add_ability_Bonuses("DEX", 2);
-        elfWoodRace.Add_ability_Bonuses("WIS", 1);
+        elfWoodRace.AddAbilityBonus("DEX", 2);
+        elfWoodRace.AddAbilityBonus("WIS", 1);
 
         Races elfDrowRace = new Races("Elf (Drow)");
-        elfDrowRace.Add_ability_Bonuses("DEX", 2);
-        elfDrowRace.Add_ability_Bonuses("CHA", 1);
+        elfDrowRace.AddAbilityBonus("DEX", 2);
+        elfDrowRace.AddAbilityBonus("CHA", 1);
 
         Races elfEladrinRace = new Races("Elf (Eladrin)");
-        elfEladrinRace.Add_ability_Bonuses("DEX", 2);
-        elfEladrinRace.Add_ability_Bonuses("INT", 1);
+        elfEladrinRace.AddAbilityBonus("DEX", 2);
+        elfEladrinRace.AddAbilityBonus("INT", 1);
 
         Races elfSeaRace = new Races("Elf (Sea)");
-        elfSeaRace.Add_ability_Bonuses("DEX", 2);
-        elfSeaRace.Add_ability_Bonuses("CON", 1);
+        elfSeaRace.AddAbilityBonus("DEX", 2);
+        elfSeaRace.AddAbilityBonus("CON", 1);
 
         Races elfShadarRace = new Races("Elf (Shadar-kai)");
-        elfShadarRace.Add_ability_Bonuses("DEX", 2);
-        elfShadarRace.Add_ability_Bonuses("CON", 1);
+        elfShadarRace.AddAbilityBonus("DEX", 2);
+        elfShadarRace.AddAbilityBonus("CON", 1);
 
         // Halfling Races 
         Races halflingLfRace = new Races("Halfling (Lightfoot)");
-        halflingLfRace.Add_ability_Bonuses("DEX", 2);
-        halflingLfRace.Add_ability_Bonuses("CHA", 1);
+        halflingLfRace.AddAbilityBonus("DEX", 2);
+        halflingLfRace.AddAbilityBonus("CHA", 1);
 
         Races halflingStoutRace = new Races("Halfling (Stout)");
-        halflingStoutRace.Add_ability_Bonuses("DEX", 2);
-        halflingStoutRace.Add_ability_Bonuses("CON", 1);
+        halflingStoutRace.AddAbilityBonus("DEX", 2);
+        halflingStoutRace.AddAbilityBonus("CON", 1);
 
         Races halflingGhostwiseRace = new Races("Halfling (Ghostwise)");
-        halflingGhostwiseRace.Add_ability_Bonuses("DEX", 2);
-        halflingGhostwiseRace.Add_ability_Bonuses("WIS", 1);
+        halflingGhostwiseRace.AddAbilityBonus("DEX", 2);
+        halflingGhostwiseRace.AddAbilityBonus("WIS", 1);
 
         // Gnome Races 
         Races gnomeForestRace = new Races("Gnome (Forest)");
-        gnomeForestRace.Add_ability_Bonuses("DEX", 1);
-        gnomeForestRace.Add_ability_Bonuses("INT", 2);
+        gnomeForestRace.AddAbilityBonus("DEX", 1);
+        gnomeForestRace.AddAbilityBonus("INT", 2);
 
         Races gnomeRockRace = new Races("Gnome (Rock)");
-        gnomeRockRace.Add_ability_Bonuses("CON", 1);
-        gnomeRockRace.Add_ability_Bonuses("INT", 2);
+        gnomeRockRace.AddAbilityBonus("CON", 1);
+        gnomeRockRace.AddAbilityBonus("INT", 2);
 
         Races gnomeDeepRace = new Races("Gnome (Deep)");
-        gnomeDeepRace.Add_ability_Bonuses("DEX", 1);
-        gnomeDeepRace.Add_ability_Bonuses("INT", 2);
+        gnomeDeepRace.AddAbilityBonus("DEX", 1);
+        gnomeDeepRace.AddAbilityBonus("INT", 2);
 
         // Orc Races 
         Races orcRace = new Races("Orc");
-        orcRace.Add_ability_Bonuses("STR", 2);
-        orcRace.Add_ability_Bonuses("CON", 1);
+        orcRace.AddAbilityBonus("STR", 2);
+        orcRace.AddAbilityBonus("CON", 1);
 
         Races halfOrcRace = new Races("Half Orc");
-        halfOrcRace.Add_ability_Bonuses("STR", 2);
-        halfOrcRace.Add_ability_Bonuses("CON", 1);
+        halfOrcRace.AddAbilityBonus("STR", 2);
+        halfOrcRace.AddAbilityBonus("CON", 1);
 
         // Tiefling Race
         Races tieflingRace = new Races("Tiefling");
-        tieflingRace.Add_ability_Bonuses("INT", 1);
-        tieflingRace.Add_ability_Bonuses("CHA", 2);
+        tieflingRace.AddAbilityBonus("INT", 1);
+        tieflingRace.AddAbilityBonus("CHA", 2);
 
         Races tieflingFeralRace = new Races("Tiefling (Feral)");
-        tieflingFeralRace.Add_ability_Bonuses("DEX", 2);
-        tieflingFeralRace.Add_ability_Bonuses("INT", 1);
+        tieflingFeralRace.AddAbilityBonus("DEX", 2);
+        tieflingFeralRace.AddAbilityBonus("INT", 1);
 
         // Aasimar Races
         Races aasimarRace = new Races("Aasimar");
-        aasimarRace.Add_ability_Bonuses("WIS", 1);
-        aasimarRace.Add_ability_Bonuses("CHA", 2);
+        aasimarRace.AddAbilityBonus("WIS", 1);
+        aasimarRace.AddAbilityBonus("CHA", 2);
 
         Races aasimarProtectorRace = new Races("Aasimar (Protector)");
-        aasimarProtectorRace.Add_ability_Bonuses("WIS", 1);
-        aasimarProtectorRace.Add_ability_Bonuses("CHA", 2);
+        aasimarProtectorRace.AddAbilityBonus("WIS", 1);
+        aasimarProtectorRace.AddAbilityBonus("CHA", 2);
 
         Races aasimarScourgeRace = new Races("Aasimar (Scourge)");
-        aasimarScourgeRace.Add_ability_Bonuses("CON", 1);
-        aasimarScourgeRace.Add_ability_Bonuses("CHA", 2);
+        aasimarScourgeRace.AddAbilityBonus("CON", 1);
+        aasimarScourgeRace.AddAbilityBonus("CHA", 2);
 
         Races aasimarFallenRace = new Races("Aasimar (Fallen)");
-        aasimarFallenRace.Add_ability_Bonuses("STR", 1);
-        aasimarFallenRace.Add_ability_Bonuses("CHA", 2);
+        aasimarFallenRace.AddAbilityBonus("STR", 1);
+        aasimarFallenRace.AddAbilityBonus("CHA", 2);
 
         // Aarakocra Race
         Races aarakocraRace = new Races("Aarakocra");
-        aarakocraRace.Add_ability_Bonuses("DEX", 2);
-        aarakocraRace.Add_ability_Bonuses("WIS", 1);
+        aarakocraRace.AddAbilityBonus("DEX", 2);
+        aarakocraRace.AddAbilityBonus("WIS", 1);
 
         // Gensai Races
         Races gensaiAirRace = new Races("Genasi (Air)");
-        gensaiAirRace.Add_ability_Bonuses("DEX", 1);
-        gensaiAirRace.Add_ability_Bonuses("CON", 2);
+        gensaiAirRace.AddAbilityBonus("DEX", 1);
+        gensaiAirRace.AddAbilityBonus("CON", 2);
 
         Races gensaiEarthRace = new Races("Genasi (Earth)");
-        gensaiEarthRace.Add_ability_Bonuses("STR", 1);
-        gensaiEarthRace.Add_ability_Bonuses("CON", 2);
+        gensaiEarthRace.AddAbilityBonus("STR", 1);
+        gensaiEarthRace.AddAbilityBonus("CON", 2);
 
         Races gensaiFireRace = new Races("Genasi (Fire)");
-        gensaiFireRace.Add_ability_Bonuses("CON", 2);
-        gensaiFireRace.Add_ability_Bonuses("INT", 1);
+        gensaiFireRace.AddAbilityBonus("CON", 2);
+        gensaiFireRace.AddAbilityBonus("INT", 1);
 
         Races gensaiWaterRace = new Races("Genasi (Water)");
-        gensaiWaterRace.Add_ability_Bonuses("CON", 2);
-        gensaiWaterRace.Add_ability_Bonuses("WIS", 1);
+        gensaiWaterRace.AddAbilityBonus("CON", 2);
+        gensaiWaterRace.AddAbilityBonus("WIS", 1);
 
         // Goliath Race 
         Races goliathRace = new Races("Goliath");
-        goliathRace.Add_ability_Bonuses("STR", 2);
-        goliathRace.Add_ability_Bonuses("CON", 1);
+        goliathRace.AddAbilityBonus("STR", 2);
+        goliathRace.AddAbilityBonus("CON", 1);
 
         // Firbolg
         Races firbolgRace = new Races("Firbolg");
-        firbolgRace.Add_ability_Bonuses("STR", 1);
-        firbolgRace.Add_ability_Bonuses("WIS", 2);
+        firbolgRace.AddAbilityBonus("STR", 1);
+        firbolgRace.AddAbilityBonus("WIS", 2);
 
         // Kenku
         Races kenkuRace = new Races("Kenku");
-        kenkuRace.Add_ability_Bonuses("DEX", 2);
-        kenkuRace.Add_ability_Bonuses("WIS",1);
+        kenkuRace.AddAbilityBonus("DEX", 2);
+        kenkuRace.AddAbilityBonus("WIS",1);
 
         // Lizardfolk 
         Races lizardFolkRace = new Races("Lizardfolk");
-        lizardFolkRace.Add_ability_Bonuses("CON", 2);
-        lizardFolkRace.Add_ability_Bonuses("WIS", 1);
+        lizardFolkRace.AddAbilityBonus("CON", 2);
+        lizardFolkRace.AddAbilityBonus("WIS", 1);
 
         // Tabaxi 
         Races tabaxiRace = new Races("Tabaxi");
-        tabaxiRace.Add_ability_Bonuses("DEX", 2);
-        tabaxiRace.Add_ability_Bonuses("CHA", 1);
+        tabaxiRace.AddAbilityBonus("DEX", 2);
+        tabaxiRace.AddAbilityBonus("CHA", 1);
 
         // Triton 
         Races tritonRace = new Races("Triton");
-        tritonRace.Add_ability_Bonuses("STR", 2);
-        tritonRace.Add_ability_Bonuses("CON", 1);
-        tritonRace.Add_ability_Bonuses("CHA", 1);
+        tritonRace.AddAbilityBonus("STR", 2);
+        tritonRace.AddAbilityBonus("CON", 1);
+        tritonRace.AddAbilityBonus("CHA", 1);
 
         // Bugbear
         Races bugBearRace = new Races("Bugbear");
-        bugBearRace.Add_ability_Bonuses("STR", 2);
-        bugBearRace.Add_ability_Bonuses("CON", 1);
+        bugBearRace.AddAbilityBonus("STR", 2);
+        bugBearRace.AddAbilityBonus("CON", 1);
 
         // Goblin Races 
         Races goblinRace = new Races("Goblin");
-        goblinRace.Add_ability_Bonuses("DEX", 2);
-        goblinRace.Add_ability_Bonuses("CON", 1);
+        goblinRace.AddAbilityBonus("DEX", 2);
+        goblinRace.AddAbilityBonus("CON", 1);
 
         Races hobGoblinRace = new Races("Hobgoblin");
-        hobGoblinRace.Add_ability_Bonuses("CON", 2);
-        hobGoblinRace.Add_ability_Bonuses("INT", 1);
+        hobGoblinRace.AddAbilityBonus("CON", 2);
+        hobGoblinRace.AddAbilityBonus("INT", 1);
 
         // Kobold Race 
         Races koboldRace = new Races("Kobold");
-        koboldRace.Add_ability_Bonuses("DEX", 2);
+        koboldRace.AddAbilityBonus("DEX", 2);
 
         // Yuan-ti Pureblood Race 
         Races yuantiPbRace = new Races("Yuan-ti Pureblood");
-        yuantiPbRace.Add_ability_Bonuses("INT", 1);
-        yuantiPbRace.Add_ability_Bonuses("CHA", 1);
+        yuantiPbRace.AddAbilityBonus("INT", 1);
+        yuantiPbRace.AddAbilityBonus("CHA", 1);
 
         // Githyanki
         Races githyankiRace = new Races("Githyanki");
-        githyankiRace.Add_ability_Bonuses("STR", 2);
-        githyankiRace.Add_ability_Bonuses("INT", 1);
+        githyankiRace.AddAbilityBonus("STR", 2);
+        githyankiRace.AddAbilityBonus("INT", 1);
 
         // Githzerai	
         Races githzeraiRace = new Races("Githzerai");
-        githzeraiRace.Add_ability_Bonuses("INT", 1);
-        githzeraiRace.Add_ability_Bonuses("WIS", 2);
+        githzeraiRace.AddAbilityBonus("INT", 1);
+        githzeraiRace.AddAbilityBonus("WIS", 2);
 
         // Tortle 
         Races tortleRace = new Races("Tortle");
-        tortleRace.Add_ability_Bonuses("STR", 2);
-        tortleRace.Add_ability_Bonuses("WIS", 1);
+        tortleRace.AddAbilityBonus("STR", 2);
+        tortleRace.AddAbilityBonus("WIS", 1);
 
         // Verdan
         Races verdanRace = new Races("Verdan");
-        verdanRace.Add_ability_Bonuses("CON", 2);
-        verdanRace.Add_ability_Bonuses("CHA", 2);
+        verdanRace.AddAbilityBonus("CON", 2);
+        verdanRace.AddAbilityBonus("CHA", 2);
 
         // Kalashtar
         Races kalashtarRace = new Races("Kalashtar");
-        kalashtarRace.Add_ability_Bonuses("CON", 2);
-        kalashtarRace.Add_ability_Bonuses("CHA", 1);
+        kalashtarRace.AddAbilityBonus("CON", 2);
+        kalashtarRace.AddAbilityBonus("CHA", 1);
 
         // Shifter Races 
         Races shifterBeasthideRace = new Races("Shifter (Beasthide)");
-        shifterBeasthideRace.Add_ability_Bonuses("STR", 1);
-        shifterBeasthideRace.Add_ability_Bonuses("CON", 2);
+        shifterBeasthideRace.AddAbilityBonus("STR", 1);
+        shifterBeasthideRace.AddAbilityBonus("CON", 2);
 
         Races shifterLongToothRace = new Races("Shifter (Longtooth)");
-        shifterLongToothRace.Add_ability_Bonuses("STR", 2);
-        shifterLongToothRace.Add_ability_Bonuses("DEX", 1);
+        shifterLongToothRace.AddAbilityBonus("STR", 2);
+        shifterLongToothRace.AddAbilityBonus("DEX", 1);
 
         Races shifterSwiftstrideRace = new Races("Shifter (Swiftstride)");
-        shifterSwiftstrideRace.Add_ability_Bonuses("DEX", 2);
-        shifterSwiftstrideRace.Add_ability_Bonuses("CHA", 1);
+        shifterSwiftstrideRace.AddAbilityBonus("DEX", 2);
+        shifterSwiftstrideRace.AddAbilityBonus("CHA", 1);
 
         Races shifterWildhuntRace = new Races("Shifter (Wildhunt)");
-        shifterWildhuntRace.Add_ability_Bonuses("DEX", 1);
-        shifterWildhuntRace.Add_ability_Bonuses("WIS", 2);
+        shifterWildhuntRace.AddAbilityBonus("DEX", 1);
+        shifterWildhuntRace.AddAbilityBonus("WIS", 2);
 
         // Centaur 
         Races centaurRace = new Races("Centaur");
-        centaurRace.Add_ability_Bonuses("STR", 2);
-        centaurRace.Add_ability_Bonuses("WIS", 1);
+        centaurRace.AddAbilityBonus("STR", 2);
+        centaurRace.AddAbilityBonus("WIS", 1);
 
         // Loxodon 
         Races loxodonRace = new Races("Loxodon");
-        loxodonRace.Add_ability_Bonuses("CON", 2);
-        loxodonRace.Add_ability_Bonuses("WIS", 1);
+        loxodonRace.AddAbilityBonus("CON", 2);
+        loxodonRace.AddAbilityBonus("WIS", 1);
 
         // Minotaur 
         Races minotaurRace = new Races("Minotaur");
-        minotaurRace.Add_ability_Bonuses("STR", 2);
-        minotaurRace.Add_ability_Bonuses("CON", 1);
+        minotaurRace.AddAbilityBonus("STR", 2);
+        minotaurRace.AddAbilityBonus("CON", 1);
 
         // Vedalken 
         Races vedalkenRace = new Races("Vedalken");
-        vedalkenRace.Add_ability_Bonuses("INT", 2);
-        vedalkenRace.Add_ability_Bonuses("WIS", 1);
+        vedalkenRace.AddAbilityBonus("INT", 2);
+        vedalkenRace.AddAbilityBonus("WIS", 1);
 
 
         // Leonin
         Races leoninRace = new Races("Leonin");
-        leoninRace.Add_ability_Bonuses("STR", 1);
-        leoninRace.Add_ability_Bonuses("CON", 2);
+        leoninRace.AddAbilityBonus("STR", 1);
+        leoninRace.AddAbilityBonus("CON", 2);
 
 
         // Satyr 
         Races SatyrRace = new Races("Satyr");
-        SatyrRace.Add_ability_Bonuses("DEX", 1);
-        SatyrRace.Add_ability_Bonuses("CHA", 2);
+        SatyrRace.AddAbilityBonus("DEX", 1);
+        SatyrRace.AddAbilityBonus("CHA", 2);
 
         // Example usage
         Console.WriteLine("Welcome to the character creation screen!");
@@ -274,7 +275,7 @@ class raceModifier
         Console.WriteLine("You've selected the " + selectedRace.Name + " race.");
         Console.WriteLine("Ability Score Bonuses:");
 
-        foreach (var bonus in selectedRace.abilityBonuses)
+        foreach (var bonus in selectedRace.AbilityBonuses)
         {
             Console.WriteLine(bonus.Key + ": " + bonus.Value);
         }
